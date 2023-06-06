0c050ce89d4de8e7332578eddf9bfb6d2e3118c8
Fatma Bellil <bellil.fa234@gmail.com>
2023-06-06 14:15:38 +0200

    Configure app, add basic API, and implement in-memory store

---
+++ src/controllers/dashboard-controller.js
@@ -0,0 +1,32 @@
+import { db } from "../models/db.js";
+
+export const dashboardController = {
+  index: {
+    handler: async function (request, h) {
+      const placemarks = await db.placemarkStore.getAllPlacemarks();
+      const viewData = {
+        title: "Placemark Dashboard",
+        placemarks: placemarks,
+      };
+      return h.view("dashboard-view", viewData);
+    },
+  },
+
+  addPlacemark: {
+    handler: async function (request, h) {
+      const newPlacemark = {
+        name: request.payload.name,
+      };
+      await db.placemarkStore.addPlacemark(newPlacemark);
+      return h.redirect("/dashboard");
+    },
+  },
+
+  deletePlacemark: {
+    handler: async function (request, h) {
+      const placemark = await db.placemarkStore.getPlacemarkById(request.params.id);
+      await db.placemarkStore.deletePlacemarkById(placemark._id);
+      return h.redirect("/dashboard");
+    },
+  },
+};
---
+++ src/models/mem/user-mem-store.js
@@ -0,0 +1,41 @@
+import { v4 } from "uuid";
+
+let users = [];
+
+export const userMemStore = {
+  async getAllUsers() {
+    return users;
+  },
+
+  async addUser(user) {
+    user._id = v4();
+    users.push(user);
+    return user;
+  },
+
+  async getUserById(id) {
+    const user = users.find((item) => item._id === id);
+    if (user === undefined) {
+        return null;
+    }
+    return user;
+
+  },
+
+  async getUserByEmail(email) {
+    const user = users.find((item) => item.email === email);
+    if (user === undefined) {
+        return null;
+    }
+    return user;
+  },
+
+  async deleteUserById(id) {
+    const index = users.findIndex((user) => user._id === id);
+    if (index !== -1) users.splice(index, 1);
+  },
+
+  async deleteAll() {
+    users = [];
+  },
+};
